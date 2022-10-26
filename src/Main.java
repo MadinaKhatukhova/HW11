7a2bd00b1240a0505d3eb4b3eae529b41829fff2
Madina <madina-khat@yandex.ru>
2022-10-26 17:54:10 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("ФИО сотрудника — %s", fullName);
     }
 }
